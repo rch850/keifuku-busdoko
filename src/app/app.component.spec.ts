290623280b8b6529a870435e2f80154a1207902e
rch850 <rich850@gmail.com>
2018-02-06 01:34:26 +0900

    💚 fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,69 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixtureAutoDetect } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { KeifukuService } from './keifuku.service';
+import { of } from 'rxjs/observable/of';
+
 describe('AppComponent', () => {
+  let keifukuServiceStub;
+
   beforeEach(async(() => {
+    keifukuServiceStub = {
+      rosenkeitobusLookup() {
+        return of({
+          rosen: [
+            {'rosenid': 1, 'rosenname': 'コミュニティバスすまいる', 'keitoid': 1, 'ikisaki': 'すまいる 田原・文京方面'},
+            {'rosenid': 1, 'rosenname': '運動公園線', 'keitoid': 2, 'ikisaki': 'すまいる 照手・足羽方面'}
+          ]
+        });
+      },
+      busstopLookup(rosenid, keitoid) {
+        return of({
+          'rosenid': rosenid,
+          'keitoid': keitoid,
+          'list': [
+            {'odr': 1, 'name': '駅前商店街'},
+            {'odr': 2, 'name': '福井市役所'},
+            {'odr': 3, 'name': '中央公園'},
+            {'odr': 4, 'name': '片町商店街'},
+            {'odr': 5, 'name': '春山二丁目'}
+          ]
+        });
+      },
+      rosenLookup(rosenid, keitoid) {
+        return of({realtime: [
+          {'rosenid': rosenid, 'keitoid': keitoid, 'lat': 36.056992, 'lon': 136.199661, 'busstopodr': 2, 'datetime': '20130201085742'}
+        ]});
+      }
+    };
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        FormsModule
       ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        { provide: KeifukuService, useValue: keifukuServiceStub },
+        { provide: ComponentFixtureAutoDetect, useValue: true }
+      ]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+
+  it('should have one viewData after search', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+    app.onSearch('中央公園');
+    expect(app.viewData.length).toBe(1);
   }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+
 });
--- src/app/keifuku.service.spec.ts
+++ src/app/keifuku.service.spec.ts
@@ -1,15 +1,48 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController, } from '@angular/common/http/testing';
 
 import { KeifukuService } from './keifuku.service';
+import { HttpBackend, JsonpClientBackend } from '@angular/common/http';
+
+// About testing JSONP request: https://github.com/angular/angular/issues/20878
 
 describe('KeifukuService', () => {
+  let keifuku: KeifukuService;
+  let httpTestingController: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [KeifukuService]
+      imports: [HttpClientTestingModule],
+      providers: [
+        KeifukuService,
+        { provide: JsonpClientBackend, useExisting: HttpBackend }
+      ]
     });
+    // Inject the http service and test controller for each test
+    httpTestingController = TestBed.get(HttpTestingController);
+    keifuku = TestBed.get(KeifukuService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', inject([KeifukuService], (service: KeifukuService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('busstopLookup', () => {
+    const expected = {
+      rosenid: 1, keitoid: 2, list: []
+    };
+    keifuku.busstopLookup(1, 2).subscribe(data => {
+      expect(data).toEqual(expected);
+    });
+
+    const req = httpTestingController.expectOne(request => {
+      return request.url === 'http://busnavi.keifuku.co.jp:81/busstopLookup.php?rosenid=1&keitoid=2';
+    });
+    expect(req.request.method).toBe('JSONP');
+    req.flush(expected);
+  });
 });
